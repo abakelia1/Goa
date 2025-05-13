2544c8eabc50ab744f4f536a76d5c78cf231c207
abakelia1 <nikanini2010@gmail.com>
2025-05-13 22:41:23 +0400

    commit

---
+++ Day 01/homework/homework1.py
@@ -0,0 +1,20 @@
+
+name = "nika"
+
+age = 14
+
+address = "bagebi"
+
+height = 180
+
+weight = 62
+
+print(name)
+print(age)
+print(address)
+print(height)
+print(weight)
+
+#declaration - დეკლარაცია - ცვლადის სახელის შეცვლა
+#inicialization - ინიციალიზება - მნისშვნელობის მინიჭება ცვლადზე
+#reinicialization- რეინიციალიზება - ცვლადს სახელში მნიშვნელობის შეცვლა 
---
+++ Day 02/homework2.py
@@ -0,0 +1,23 @@
+number1 = 17
+number2 = 22
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 // number2)
+print(number1 / number2)
+
+birht_year = 2010
+current_year = 2025
+print(current_year - birht_year)
+
+age = 15
+
+number1 = 8
+number2 = 10
+number3 = 7
+number4 = 26
+number5 = 13
+print( number1 + number2 + number3 + number4 + number5 )
+print(64 / 5)
+print(64 // 5)
