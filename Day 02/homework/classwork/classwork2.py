2cd042966b74d7b58de4d651fbf5d797893f261d
abakelia1 <nikanini2010@gmail.com>
2025-05-30 17:52:12 +0400

    commit

---
+++ Day 02/homework/classwork/classwork2.py
@@ -0,0 +1,8 @@
+n1 = 47
+n2 = 38
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 //n2)
---
+++ Day 04/classwork/homework/homework.py
@@ -0,0 +1,34 @@
+sityva1 = "nika"
+sityva2 = 14
+sityva3 =  "wlisaa"
+
+
+
+
+
+
+nivti = "kivi"
+sityva = "girs"
+fasi = 3.5
+kupiura = "lari"
+print(nivti+sityva+fasi+kupiura)
+
+
+
+sigrdze = 3.2
+sigane  = 2.4
+print(sigrdze * sigane)
+
+
+
+number1 = 167
+number2 = 155
+print(number1+number2)
+
+
+number1 = 1756
+number2 = 9
+print(number1/number2)
+
+
+
